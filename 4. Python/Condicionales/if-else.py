43c6dd0f84e0aeaf18a317e9dcb95e4eccb795ff
Josue Soto <zte-2000@hotmail.com>
2023-12-04 19:03:26 -0600

    comparaciones, operaciones y if-else

---
+++ 4. Python/Condicionales/if-else.py
@@ -0,0 +1,34 @@
+"""
+if condicion is TRUE:
+    accion
+if conticion is FALSE:
+    accion
+"""
+##Ejemplo 1
+edad = 19
+#Las identaciones son OBLIGATORIAS para que funcione else and if
+if edad >= 18:
+    print("Es mayor de edad")
+else:
+    print("Eres menor de edad")
+
+
+##Ejemplo 2  
+contraseña_bs = "josveS"
+contraseña_escrita = "Josve"
+
+if contraseña_bs == contraseña_escrita:
+    print("Bievenido")
+else:
+    print("Acceso denegado :)")
+    
+
+##Ejemplo 3
+##Es posible meter un if dentro de otro if, if anidados
+ingreso_mensual = 21000
+if ingreso_mensual >= 30000:
+    print("Estás bien en cualquier parte del mundo")
+elif ingreso_mensual >= 20000:
+    print("Estás bien en latam")
+else:
+    print("No hay ni para el pan bro")
